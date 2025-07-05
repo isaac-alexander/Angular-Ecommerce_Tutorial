a0b5bf7d47b4feafd232d58a4cfa53d74c9d03bf
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-05 02:15:08 +0100

    src/app/components/

---
+++ src/app/components/header/header.ts
@@ -0,0 +1,22 @@
+import { Component, signal } from '@angular/core';
+import { PrimaryButton } from "../primary-button/primary-button";
+
+@Component({
+  selector: 'app-header',
+  imports: [PrimaryButton],
+  template: `
+  <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center">
+    <span class="text-xl">My Store </span>
+    <app-primary-button label="Cart" (btnClicked)="showButtonClicked()"/>
+  </div>
+  `,
+  styles: `
+  
+  `
+})
+export class Header {
+  showButtonClicked() {
+    console.log('clicked');
+    
+  }
+}
---
+++ src/app/components/primary-button/primary-button.ts
@@ -0,0 +1,17 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  imports: [],
+  template: `
+  <button class="cursor-pointer bg-blue-500 text-white w-full border px-5 py-2 rounded-xl shadow-md hover:opacity-90" (click)="btnClicked.emit()">
+    {{ label() }}
+  </button>
+  `,
+  styles: ``
+})
+export class PrimaryButton {
+  label = input('');
+
+  btnClicked = output();
+}
