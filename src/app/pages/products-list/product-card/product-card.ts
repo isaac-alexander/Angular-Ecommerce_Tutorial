04879f474239464822c6e5e0bd50e3841edfdf9a
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-05 02:15:53 +0100

    src/app/pages/

---
+++ src/app/pages/products-list/product-card/product-card.ts
@@ -0,0 +1,32 @@
+import { Component, input } from '@angular/core';
+import { Product } from '../../../models/products.model';
+import { PrimaryButton } from "../../../components/primary-button/primary-button";
+
+@Component({
+  selector: 'app-product-card',
+  imports: [PrimaryButton],
+  template: `
+      <div class="bg-white shadow-md/30 border-white rounded-xl p-6 flex flex-col gap-g relative">
+        <div class="mx-auto">
+          <img [src]="product().image" class="w-[200px] h[100px] object-contain">
+          <div class="flex flex-col mt-2">
+            <span class="text-md font-bold">{{ product().title }}</span>
+            <span class="text-sm">{{ '$' + product().price }}</span>
+            <app-primary-button label="Add to Cart" class="mt-3"/>
+          </div>
+
+          <span class="absolute top-2 right-3 text-sm font-bold" [class]="product().stock ? 'text-green-500' :'text-red-500'">
+            @if (product().stock) {
+              {{product().stock}} left
+            } @else {
+              Out of stock
+            }
+          </span>
+        </div>
+      </div>
+  `,
+  styles: ``
+})
+export class ProductCard {
+  product = input.required<Product>();
+}
