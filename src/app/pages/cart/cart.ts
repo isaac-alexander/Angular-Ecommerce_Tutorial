23a142b2b8abdc385ce5cccd5ed149f2a1a54aff
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-06 02:12:11 +0100

    src/app/pages/cart/

---
+++ src/app/pages/cart/cart.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { CartItem } from "./cart-item/cart-item";
+
+@Component({
+  selector: 'app-cart',
+  imports: [CartItem],
+  template: `
+    <div class="p-6 flex flex-col gap-4">
+      <h2 class="text-2xl">Shopping Cart</h2>
+      @for (item of cartService.cart(); track item.id) {
+        <app-cart-item [item] ="item" />
+      }
+    </div>
+  `,
+  styles: ``
+})
+export class Cart {
+
+  cartService = inject(CartService);
+
+}
