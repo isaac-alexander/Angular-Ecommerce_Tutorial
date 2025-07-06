a1d0e48d712bdd433218a3f4bcba7a9d25267171
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-06 02:11:17 +0100

    src/app/pages/products-list/product-card/product-card.ts

--- src/app/pages/products-list/product-card/product-card.ts
+++ src/app/pages/products-list/product-card/product-card.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Product } from '../../../models/products.model';
 import { PrimaryButton } from "../../../components/primary-button/primary-button";
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-product-card',
@@ -12,7 +13,7 @@ import { PrimaryButton } from "../../../components/primary-button/primary-button
           <div class="flex flex-col mt-2">
             <span class="text-md font-bold">{{ product().title }}</span>
             <span class="text-sm">{{ '$' + product().price }}</span>
-            <app-primary-button label="Add to Cart" class="mt-3"/>
+            <app-primary-button label="Add to Cart" class="mt-3" (btnClicked)="cartService.addToCart(product())" />
           </div>
 
           <span class="absolute top-2 right-3 text-sm font-bold" [class]="product().stock ? 'text-green-500' :'text-red-500'">
@@ -28,5 +29,8 @@ import { PrimaryButton } from "../../../components/primary-button/primary-button
   styles: ``
 })
 export class ProductCard {
+
+  cartService = inject(CartService);
+
   product = input.required<Product>();
 }
